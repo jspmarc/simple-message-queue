6758989d648b717450c42930dc7e9a61f78a43fc
Josep Marcello <jspmarcello@live.com>
2023-03-26 15:22:18 +0700

    feat(client): add multi-threaded client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,8 @@
 use crate::client::ClientImpl;
+use log::info;
 use smq_lib::structs::message::Message;
 use smq_lib::traits::client::Client;
-use std::thread::sleep;
+use std::thread;
 use std::time::Duration;
 
 mod client;
@@ -10,22 +11,34 @@ fn main() {
     ::std::env::set_var("RUST_LOG", "INFO");
     env_logger::init();
 
-    let mut client = ClientImpl::new();
+    let mut threads: Vec<thread::JoinHandle<()>> = vec![];
+    for id in 0..2000 {
+        let t = thread::spawn(move || {
+            let mut client = ClientImpl::new();
 
-    client
-        .connect("localhost", 8080)
-        .expect("Can't connect to server");
+            client
+                .connect("localhost", 8080)
+                .expect("Can't connect to server");
 
-    let msg = Message::from_u16_arr(&[1, 2]);
-    let result = client.push(&msg).expect("Can't push message");
-    if !result {
-        panic!("Failed to push message, server can't receive");
+            if id % 2 == 0 {
+                let msg = Message::from_i32_arr(&[id]);
+                let result = client.push(&msg).expect("Can't push message");
+                if !result {
+                    panic!("Failed to push message, server can't receive");
+                }
+                info!("client id {} has pushed a message", id);
+            } else {
+                let result = client.pull().expect("Can't pull message");
+                println!("client id {} pulled:\n{:#?}", id, result);
+            }
+
+            thread::sleep(Duration::from_millis(100));
+            client.disconnect().expect("Can't disconnect");
+        });
+        threads.push(t);
     }
 
-    sleep(Duration::from_secs(1));
-
-    let result = client.pull().expect("Can't pull message");
-    println!("{:#?}", result);
-
-    client.disconnect().expect("Can't disconnect");
+    for t in threads {
+        let _ = t.join();
+    }
 }
